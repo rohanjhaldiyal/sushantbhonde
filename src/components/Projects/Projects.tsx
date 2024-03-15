353bb13e66da078cad5dbea576e5bd22fc335917
Rohan Jhaldiyal <rohanjhaldiyal@gmail.com>
2024-03-16 02:27:27 +0530

    Refactor code to fetch projects from Notion database

--- src/components/Projects/Projects.tsx
+++ src/components/Projects/Projects.tsx
@@ -1,14 +1,46 @@
 import { FaComputer } from "react-icons/fa6";
 import ProjectsCard from "./ProjectsCard";
+import { Client } from "@notionhq/client";
+import { PageObjectResponse } from "@notionhq/client/build/src/api-endpoints";
+
+const notionSecret = process.env.NOTION_SECRET;
+const notionDatabaseId = process.env.NOTION_PROJECTS_DATABASE_ID;
+
+const notion = new Client({ auth: notionSecret });
 
 async function fetchProjects() {
-  const API_URL = process.env.API_URL;
-  const res = await fetch(`${API_URL}/api/notion/projects`);
-  if (!res.ok) {
-    throw new Error("Failed to fetch data");
+  if (!notionSecret || !notionDatabaseId) {
+    throw new Error("Notion secret or database id not provided");
   }
 
-  return res.json();
+  const response = await notion.databases.query({
+    database_id: notionDatabaseId,
+    sorts: [
+      {
+        property: "index",
+        direction: "ascending",
+      },
+    ],
+  });
+
+  type Project = {
+    title: string;
+    description: string;
+    link: string;
+    coverImageUrl: string;
+  };
+  const filteredResponse: Project[] = response.results.map((page) => ({
+    title:
+      (page as PageObjectResponse).properties.title?.title[0]?.text.content ||
+      "",
+    description: (page as PageObjectResponse).properties.description
+      .rich_text[0].text.content,
+    link: (page as PageObjectResponse).properties.link.url,
+    coverImageUrl: (page as PageObjectResponse).properties.cover_image.files[0]
+      ?.file.url,
+  }));
+
+  return filteredResponse;
 }
 
 export default async function Projects(): Promise<any> {
